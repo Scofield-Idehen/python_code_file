fa0afd8559e6b2239a8ce577cbaf767a8c0b6f2c
“Scofield” <Scofields109@gmail.com>
2023-10-02 12:06:30 +0100

    revamp

--- building.py
+++ building.py
@@ -1,18 +1,31 @@
 import json
-
-data = json.load(open("data.json"))
 from difflib import get_close_matches
 
-def translate(w):
-    #w =w.lower()
-    if w in data:
-        return data[w]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        return f"Did you mean {get_close_matches(w, data.keys())[0]}"
+while True:
+
+    data = json.load(open('data.json'))
+
+
+    def new_project(f):
+        f = f.lower()
+        if f in data:
+            return data[f]
+        elif len(get_close_matches(f, data.keys())) > 0:
+            user_answer = input(f"Do you mean {get_close_matches(f, data.keys())[0]}?:if Yes Enter 'Y' or 'N: " )
+            if user_answer.upper() == "Y":
+                return f'{data[get_close_matches(f, data.keys())[0]]}'
+            elif user_answer.upper() == 'N':
+                return f'{write} is not in this dictionary, please check again: '
+            else:
+                return 'Unknow term!!!'
+
+        else:
+            return f'this {write} not found'
+
+    write = input("Input your word: ")
+    news = new_project(write)
+    if type(news) == list:
+        for i in news:
+            print(i)
     else:
-        return f"{word} is not an english word, check your spelling!!!"
-
-
-word = input("Enter Word: ")
-ab = translate(word.lower())
-print(ab)
+        print(news)
